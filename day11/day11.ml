de19aab4abb1e5be30faca12bf23c6f372491c56
Qianmian Yu <im.qianmian.yu@gmail.com>
2024-12-13 00:26:04 +0800

    Day 11: Combine the Memo.recursive usage

--- day11/day11.ml
+++ day11/day11.ml
@@ -1,21 +1,21 @@
 open Core
 
-let blink_nonrecursive blink (stone, count) =
-    if count = 0 then
-        1
-    else if stone = 0 then
-        blink (1, (count - 1))
-    else
-        let s = Int.to_string stone in
-        let len = String.length s in
-        if len mod 2 = 0 then
-            [ String.slice s 0 (len / 2); String.slice s (len / 2) len ]
-            |> List.map ~f:Int.of_string
-            |> List.fold ~init:0 ~f:(fun acc x -> acc + (blink (x, (count - 1))))
+let blink = Memo.recursive ~hashable:Hashtbl.Poly.hashable
+    (fun blink (stone, count) ->
+        if count = 0 then
+            1
+        else if stone = 0 then
+            blink (1, (count - 1))
         else
-            blink ((stone * 2024), (count - 1))
+            let s = Int.to_string stone in
+            let len = String.length s in
+            if len mod 2 = 0 then
+                [ String.slice s 0 (len / 2); String.slice s (len / 2) len ]
+                |> List.map ~f:Int.of_string
+                |> List.fold ~init:0 ~f:(fun acc x -> acc + (blink (x, (count - 1))))
+            else
+                blink ((stone * 2024), (count - 1)))
 
-let blink = Memo.recursive ~hashable:Hashtbl.Poly.hashable blink_nonrecursive
 
 let part1 stones =
     List.fold stones ~init:0 ~f:(fun acc stone -> acc + (blink (stone, 25)))
